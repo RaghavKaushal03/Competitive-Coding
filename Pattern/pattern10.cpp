81497f03efe7960ddb436ecd60ca65c25a57d932
RaghavKaushal03 <kaushal.raghav13@gmail.com>
2021-01-03 03:51:03 +0530

    imp

---
+++ Pattern/pattern10.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	int n;
+	cout<<"Enter a number"<<endl;
+	cin>>n;
+	int k=0;
+	// let z = (n+1)/2;
+	cout<<"You entered "<<n<<endl;
+	for(int i=1;i<=n;i++)
+	{
+		i<=(n+1)/2?k++:k--;
+		for(int j=1;j<=n;j++){
+			if(j>=((n+1)/2-k+1) && j<=((n+1)/2+k-1)){
+				cout<<"*";
+			}
+			else{
+				cout<<" ";
+				
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
