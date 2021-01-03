5689ae804bcae4bb11af6fb0b9203537e7be2108
RaghavKaushal03 <kaushal.raghav13@gmail.com>
2021-01-03 17:43:11 +0530

    Efficient solution added

--- Pattern/pattern17.cpp
+++ Pattern/pattern17.cpp
@@ -5,8 +5,7 @@ int main(int argc, char const *argv[])
 	int n;
 	cout<<"Enter a number"<<endl;
 	cin>>n;
-	int mid = (n+1)/2;
-	int p=mid,q=mid;
+	int p=(n+1)/2,q=(n+1)/2;
 	cout<<"You entered "<<n<<endl;
 	for(int i=1;i<=n;i++)
 	{
@@ -14,12 +13,11 @@ int main(int argc, char const *argv[])
 			if(j<=p || j>=q){
 				cout<<"*";
 			}
-		
 			else{
 				cout<<" ";
 			}
 		}
-		if(i<mid){
+		if(i<(n+1)/2){
 			p--;
 			q++;
 		}
@@ -32,3 +30,31 @@ int main(int argc, char const *argv[])
 	}
 	return 0;
 }
+
+
+
+// <---- REDUCED VARIABLES  + LESS LINES SOLUTION---->
+// #include<iostream>
+// using namespace std;
+// int main(int argc, char const *argv[])
+// {
+// 	int n;
+// 	cout<<"Enter a number"<<endl;
+// 	cin>>n;
+// 	int k=0;
+// 	cout<<"You entered "<<n<<endl;
+// 	for(int i=1;i<=n;i++)
+// 	{
+// 		i<=(n+1)/2?k++:k--;
+// 		for(int j=1;j<=n;j++){
+// 			if(j<=(n+1)/2-k+1 || j>=(n+1)/2+k-1){
+// 				cout<<"*";
+// 			}
+// 			else{
+// 				cout<<" ";
+// 			}
+// 		}
+// 		cout<<endl;
+// 	}
+// 	return 0;
+// }
